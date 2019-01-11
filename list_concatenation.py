080e9460069b9fd6bc63a1f5b231a6f30fa71447
imamhossainroni <imamhossainroni95@gmail.com>
2019-01-11 18:49:27 +0600

    fix variable issue-1

--- list_concatenation.py
+++ list_concatenation.py
@@ -1,12 +1,12 @@
-catnmaes =[]
+catnames =[]
 
 while True:
-    print('Enter the name of cat ' + str(len(catnmaes) + 1) + '(Or enter nothing to stop.):')
+    print('Enter the name of cat ' + str(len(catnames) + 1) + '(Or enter nothing to stop.):')
     name = input()
 
     if name == '':
         break
-    catnmaes = catnmaes +[name]
+    catnames = catnames +[name]
 print('The cat names are:')
-for name in catnmaes:
+for name in catnames:
     print(' '+ name)
