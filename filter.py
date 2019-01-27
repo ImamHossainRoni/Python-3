98b9a1d7ea146eb613bc6a14be35f28582c1c98d
imamhossainroni <imamhossainroni95@gmail.com>
2019-01-27 23:44:54 +0600

    Used Lambda instead of using function

---
+++ filter.py
@@ -0,0 +1,10 @@
+def is_even(n):
+    return n%2 ==0
+
+nums = [45,67,23,3,7,8,90,44]
+evens = list(filter(is_even,nums))
+print(evens)
+
+#Used Lambda instead of using function
+odds = list(filter(lambda n:n%2==1,nums))
+print(odds)
