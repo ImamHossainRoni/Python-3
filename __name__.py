2b209fad156c4089c6f92f601921a9f38db7c835
imamhossainroni <imamhossainroni95@gmail.com>
2019-02-02 23:56:00 +0600

    __name__

--- __name__.py
+++ __name__.py
@@ -1,1 +1,12 @@
 print(__name__)
+def fun1():
+    print("from fun 1")
+def fun2():
+    print("from fun 2")
+
+def main():
+    fun1()
+    fun2()
+    print("from main function",__name__)
+
+main()
